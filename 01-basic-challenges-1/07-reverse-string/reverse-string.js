3c3c453ad43453304b0171703f566845976f69a2
igorntwari <igorntwari28@gmail.com>
2024-03-05 15:42:12 +0200

    feat:solved challenge 08

--- 01-basic-challenges-1/07-reverse-string/reverse-string.js
+++ 01-basic-challenges-1/07-reverse-string/reverse-string.js
@@ -1,3 +1,10 @@
-const reverseString = (str)=> str.split('').reverse().join('')
+// const reverseString = (str)=> str.split('').reverse().join('')
 
+function reverseString(str) {
+    let reversed = ''
+    for(let i = str.length-1; i >=0; i--) {
+        reversed += str[i]
+    }
+    return reversed
+}
 module.exports = reverseString;
--- 01-basic-challenges-1/08-palindrome/palindrome.js
+++ 01-basic-challenges-1/08-palindrome/palindrome.js
@@ -1,3 +1,3 @@
-function isPalindrome() {}
+const isPalindrome = (str)=>  str.toLowerCase().replace(/[^a-z0-9]/g, '') === str.split('').reverse().join('') ? true: false
 
 module.exports = isPalindrome;
