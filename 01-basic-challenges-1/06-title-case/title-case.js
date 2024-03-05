c6c61816bc4a3b3cfbceeb8d713bdc9c1fea235b
igorntwari <igorntwari28@gmail.com>
2024-03-05 15:00:42 +0200

    feat:solved challenge 06

--- 01-basic-challenges-1/06-title-case/title-case.js
+++ 01-basic-challenges-1/06-title-case/title-case.js
@@ -1,3 +1,9 @@
-function titleCase() {}
+function titleCase(str) {
+   const word =  str.toLowerCase().split(' ')
+   for(let i = 0; i < word.length; i++) {
+    word[i] = word[i][0].toUpperCase() + word[i].slice(1)
+   }
+return word.join(' ')
+}
 
 module.exports = titleCase;
