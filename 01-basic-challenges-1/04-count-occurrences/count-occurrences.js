3e173ea87bc299d7fb3323995220e79a9f128d5a
igorntwari <igorntwari28@gmail.com>
2024-03-05 12:14:07 +0200

    feat:solved challenge 04

--- 01-basic-challenges-1/04-count-occurrences/count-occurrences.js
+++ 01-basic-challenges-1/04-count-occurrences/count-occurrences.js
@@ -1,3 +1,12 @@
-function countOccurrences() {}
+function countOccurrences(string,char) {
+    // let sentence = string.toLowerCase().split('')
+    let count = 0
+    for (let i = 0; i < string.length; i++) {
+     if(string[i] == char) {
+        count++
+     }
+    }
+    return count
+}
 
 module.exports = countOccurrences;
