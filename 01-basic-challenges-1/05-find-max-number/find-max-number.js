32b33f7d78bbb4c2ec0c64178946f76e1ca2b52a
igorntwari <igorntwari28@gmail.com>
2024-03-05 12:41:47 +0200

    feat:solved challenge 05

--- 01-basic-challenges-1/05-find-max-number/find-max-number.js
+++ 01-basic-challenges-1/05-find-max-number/find-max-number.js
@@ -1,3 +1,10 @@
-function findMaxNumber() {}
+const findMaxNumber = (arr)=> {
+    return arr.reduce((prevValue,curretValue)=> {
+    if(prevValue > curretValue) {
+        return prevValue
+    }
+    return curretValue
+
+},0)}
 
 module.exports = findMaxNumber;
