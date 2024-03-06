9f6f20e0ea9fc259ed69c3f951b70cf36ffb17e5
igorntwari <igorntwari28@gmail.com>
2024-03-06 16:51:53 +0200

    feat:finding missing letter

--- 02-basic-challenges-2/04-find-missing-number/find-missing-number.js
+++ 02-basic-challenges-2/04-find-missing-number/find-missing-number.js
@@ -1,3 +1,11 @@
-function findMissingNumber() {}
+function findMissingNumber(numbers) {
+    let startingPoint = numbers.indexOf(numbers[0])
+    for (let i = 0 ; i<numbers.length; i++) {
+     if(numbers[i] !== numbers[startingPoint + i]) {
+         return aplaha[startingPoint + i]
+     }
+    }
+    return ""
+}
 
 module.exports = findMissingNumber;
--- 02-basic-challenges-2/05-find-missing-letter/find-missing-letter.js
+++ 02-basic-challenges-2/05-find-missing-letter/find-missing-letter.js
@@ -1,3 +1,12 @@
-function findMissingLetter() {}
+function findMissingLetter(letters) {
+    const aplaha = 'abcdefghijklmnopqrstuvwxyz'
+   let startingPoint = aplaha.indexOf(letters[0])
+   for (let i = 0 ; i<letters.length; i++) {
+    if(letters[i] !== aplaha[startingPoint + i]) {
+        return aplaha[startingPoint + i]
+    }
+   }
+   return ""
+}
 
 module.exports = findMissingLetter;
