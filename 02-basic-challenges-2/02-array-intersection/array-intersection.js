645087bc989ea8bcf5d0671ebf6eca2df5a1ac4b
igorntwari <igorntwari28@gmail.com>
2024-03-06 10:38:55 +0200

    feat:solved array-intersection

--- 02-basic-challenges-2/02-array-intersection/array-intersection.js
+++ 02-basic-challenges-2/02-array-intersection/array-intersection.js
@@ -1,3 +1,11 @@
-function arrayIntersection() {}
+function arrayIntersection(arr1,arr2) {
+    let intersectionArr = []
+    for(let i = 0 ; i<arr1.length; i++) {
+      if(arr2.includes(arr1[i]) && !intersectionArr.includes(arr1[i])){
+        intersectionArr.push(arr1[i])
+      }
+    }
+    return intersectionArr
+}
 
 module.exports = arrayIntersection;
