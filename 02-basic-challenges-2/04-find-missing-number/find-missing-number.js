818fe4613c97a8aacf5d8d08c42420ab22cd7cd3
igorntwari <igorntwari28@gmail.com>
2024-03-06 18:45:22 +0200

    faet:solved find-missing-number

--- 02-basic-challenges-2/04-find-missing-number/find-missing-number.js
+++ 02-basic-challenges-2/04-find-missing-number/find-missing-number.js
@@ -1,11 +1,14 @@
-function findMissingNumber(numbers) {
-    let startingPoint = numbers.indexOf(numbers[0])
-    for (let i = 0 ; i<numbers.length; i++) {
-     if(numbers[i] !== numbers[startingPoint + i]) {
-         return aplaha[startingPoint + i]
-     }
-    }
-    return ""
+const findMissingNumber = (numbers)=> {
+    if (!numbers || numbers.length === 0) {
+        return undefined;
+      }
+ const n = numbers.length + 1
+ let expectedSum = (n * (n + 1)) / 2
+ let actualSum = 0
+ for (let i = 0 ;i<numbers.length; i++) {
+  actualSum +=numbers[i]  
+ }
+ return expectedSum - actualSum
 }
 
 module.exports = findMissingNumber;
