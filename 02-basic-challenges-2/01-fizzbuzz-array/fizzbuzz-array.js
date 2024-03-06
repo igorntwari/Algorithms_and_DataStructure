75be4525bf8788baea7b9cdc2af05bfc41e06abb
igorntwari <igorntwari28@gmail.com>
2024-03-06 10:19:02 +0200

    feat:solved challenge part02-01

--- 02-basic-challenges-2/01-fizzbuzz-array/fizzbuzz-array.js
+++ 02-basic-challenges-2/01-fizzbuzz-array/fizzbuzz-array.js
@@ -1,3 +1,22 @@
-function fizzBuzzArray() {}
+const fizzBuzzArray = (n) => {
+    console.log(n)
+    const arr = []
+  for (let i = 1; i <= n; i++) {
+
+     if (i % 3 === 0 && i % 5 === 0) {
+        arr.push("FizzBuzz");
+      } 
+   else if (i % 3 === 0) {  
+        arr.push("Fizz");
+    } 
+   else if (i % 5 === 0){
+        arr.push("Buzz");
+    } else {
+
+        arr.push(i)
+  }
+}
+return arr
+};
 
 module.exports = fizzBuzzArray;
