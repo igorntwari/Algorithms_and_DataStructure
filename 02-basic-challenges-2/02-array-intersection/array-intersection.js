f21dbd51effd8953b4fc36e7e79dc03bf3397c0b
igorntwari <igorntwari28@gmail.com>
2024-03-06 11:31:31 +0200

    feat:solved display likes

--- 02-basic-challenges-2/02-array-intersection/array-intersection.js
+++ 02-basic-challenges-2/02-array-intersection/array-intersection.js
@@ -1,9 +1,12 @@
 function arrayIntersection(arr1,arr2) {
     let intersectionArr = []
     for(let i = 0 ; i<arr1.length; i++) {
-      if(arr2.includes(arr1[i]) && !intersectionArr.includes(arr1[i])){
-        intersectionArr.push(arr1[i])
-      }
+     for(let j = 0 ; j<arr2.length; j++){
+        if(arr1[i] === arr2[j]) {
+            intersectionArr.push(arr1[i])
+          break
+        }
+     }
     }
     return intersectionArr
 }
--- 02-basic-challenges-2/03-display-likes/display-likes.js
+++ 02-basic-challenges-2/03-display-likes/display-likes.js
@@ -1,3 +1,9 @@
-function displayLikes() {}
+function displayLikes(namesArr) {
+  if(namesArr.length > 3)   return `${namesArr[0]}, ${namesArr[1]} and ${namesArr.length - 2} others like this`
+  if(namesArr.length ===3)  return `${namesArr[0]}, ${namesArr[1]} and ${namesArr[2]} like this`
+  if(namesArr.length === 2) return `${namesArr[0]} and ${namesArr[1]} like this`
+  if(namesArr.length === 1) return `${namesArr[0]} likes this`
+  if(namesArr.length === 0) return `no one likes this`
 
+}
 module.exports = displayLikes;
