0cbc00068f845ca03c078ab76346a68fae0c6740
igorntwari <igorntwari28@gmail.com>
2024-03-07 15:43:11 +0200

    feat:first-non-repeating

--- 02-basic-challenges-2/07-first-non-repeating/first-non-repeating.js
+++ 02-basic-challenges-2/07-first-non-repeating/first-non-repeating.js
@@ -1,14 +1,28 @@
 function findFirstNonRepeatingCharacter(str) {
-    for(let i = 0; i<str.length; i++) {
-        for (let j = 0; j<str.length; j++) {
-            if(str[i] === str[j] && i !== j) {
-                break
-            } else if(j+1 === str.length) {
-                return str[i]
-            }
-        }
+ const count = {}
+
+ str.split('').forEach(elt => {
+    if(count.hasOwnProperty(elt)){
+        count[elt]++
+    }else{
+        count[elt]=1
     }
+ })
+
+const igor = Object.entries(count)
+
+if(igor.every(([,elt]) => elt > 1)){
     return null
+}
+
+for(let i = 0; i <= igor.length; i++){
+    const [key, value] = igor[i]
+
+    if(value === 1){
+        return key
+    }
+}
+ 
 }
 
 module.exports = findFirstNonRepeatingCharacter;
