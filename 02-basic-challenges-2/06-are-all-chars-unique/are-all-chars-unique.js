38003ca62bd70c764bc262e9461b89d60cb3b589
igorntwari <igorntwari28@gmail.com>
2024-03-07 10:11:13 +0200

    feat:all-chars-unique

--- 02-basic-challenges-2/06-are-all-chars-unique/are-all-chars-unique.js
+++ 02-basic-challenges-2/06-are-all-chars-unique/are-all-chars-unique.js
@@ -1,3 +1,16 @@
-function areAllCharactersUnique() {}
+function areAllCharactersUnique(str) {
+// for(let i = 0 ; i<str.length; i++) {
+//   for(let j = i+1; j<str.length;j++){
+//     if(str[i] === str[j]) {
+//         console.log(str[i],str[j],str[i]===str[j])
+//        return false
+//     }
+//   }
+// }
+// return true
+}
 
+const uniques = Array.from(new Set(str.split(''))).join('')
+return str ===uniques;
 module.exports = areAllCharactersUnique;
+'programming'
