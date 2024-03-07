2b750281d2b69f08e23d1a7f89f5fb08d392bca0
igorntwari <igorntwari28@gmail.com>
2024-03-07 11:15:54 +0200

    feat:solved first-non-repeating

--- 02-basic-challenges-2/07-first-non-repeating/first-non-repeating.js
+++ 02-basic-challenges-2/07-first-non-repeating/first-non-repeating.js
@@ -1,3 +1,14 @@
-function findFirstNonRepeatingCharacter() {}
+function findFirstNonRepeatingCharacter(str) {
+    for(let i = 0; i<str.length; i++) {
+        for (let j = 0; j<str.length; j++) {
+            if(str[i] === str[j] && i !== j) {
+                break
+            } else if(j+1 === str.length) {
+                return str[i]
+            }
+        }
+    }
+    return null
+}
 
 module.exports = findFirstNonRepeatingCharacter;
